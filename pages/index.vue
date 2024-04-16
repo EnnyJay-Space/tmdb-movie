45dfa089646c885bf62b85a9db722a723b8f4966
ennyworld <ennyjay4you@gmail.com>
2024-04-16 13:22:58 +0100

    installed tailwind css

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mb-8">
+        <NavBar />
+    </div>
+
+    <v-container>
+        <div class="card">
+            <div class="card-header">
+                <h4>Student List
+                    <NuxtLink to="/students/create" class="btn btn-primary float-end">Add Student</NuxtLink>
+                </h4>
+            </div>
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </v-container>
+</template>
+
+<script setup>
+// definePageMeta({
+//     middleware: ["auth-check"]
+// })
+</script>
+
+<style lang="scss" scoped></style>
