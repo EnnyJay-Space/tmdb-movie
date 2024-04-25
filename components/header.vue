547ff2b0a51d877724d55cdc316561ef0c3baf29
ennyworld <ennyjay4you@gmail.com>
2024-04-25 11:40:26 +0100

    updated

---
+++ components/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mx-auto flex max-w-7xl bg-[url('/assets/img/hero.png')]">
+        <h1>Welcome</h1>
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+/* .hero-section{
+    background-image: ;
+} */
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,10 @@
 <template>
     <div class="mb-8">
         <NavBar />
+        <Header />
     </div>
 
-    <v-container>
-        <div class="card">
-            <div class="card-header">
-                <h4>Student List
-                    <NuxtLink to="/students/create" class="btn btn-primary float-end">Add Student</NuxtLink>
-                </h4>
-            </div>
-            <div class="card-body">
-                
-            </div>
-        </div>
-    </v-container>
+   
 </template>
 
 <script setup>
